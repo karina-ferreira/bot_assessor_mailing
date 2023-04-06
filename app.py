88b69f0ed8817e9b94320d2f756f98bd5920895b
Karina Ferreira <108735210+karina-ferreira@users.noreply.github.com>
2023-04-05 23:46:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-datetime
-flask
-gspread
-gunicorn
-oauth2client
-requests
-request
-time
+import os
+
+import datetime
+import getpass
+import gspread
+import json
+import pandas as pd
+import requests
+import sendgrid
+import time
+
+from flask import Flask, request
+from io import StringIO 
+from oauth2client.service_account import ServiceAccountCredentials
+from sendgrid.helpers.mail import Mail, Email, To, Content
+
+
+# ______________________________ site
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return "Esse é o site do Bot Assessor de Mailing. Para conferir a documentação acesse: https://github.com/karina-ferreira/bot_assessor_mailing"
