6cccd2752418f486fd425bc1c8333dfcb4c1402e
Karina Ferreira <108735210+karina-ferreira@users.noreply.github.com>
2023-04-06 18:16:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -37,47 +37,46 @@ app = Flask(__name__)
 def index():
   return "Esse é o site do Bot Assessor de Mailing. Para conferir a documentação acesse: https://github.com/karina-ferreira/bot_assessor_mailing"
 
-# ______________________________ bot ___________________________________________
+# ______________________________ bot (teste quase completo) ___________________________________________ 
 
 @app.route("/telegram-bot", methods=['POST'])
 def telegram_bot():
-  mensagens = []
-
-  update = request.json
+    mensagens = []
   
-  if request.method == 'POST':
-     update = request.get_json()
-    
-  update_id = update['update_id']
-  first_name = update['message']['from']['first_name']
-  last_name = update['message']['from']['last_name']
-  user_name = update['message']['from']['username']
-  sender_id = update['message']['from']['id']
-  date = datetime.fromtimestamp(update['message']['date']).date()
-  time = datetime.fromtimestamp(update['message']['date']).time()
-  chat_id = update['message']['chat']['id']
-  
-  try:
-      message = update['message']['text']
-  except KeyError:
-      print("erro")
-      message=''
+    if request.method == 'POST':
+        update = request.get_json()
 
-  #return chat_id, texto
-  
-  if "username" in update["message"]["from"]:
-    username = f' @{update["message"]["from"]["username"]}'
-  else:
-    username = ""
+        # Defina a lista de usuários permitidos
+        usuarios_permitidos = ["kuaraina", "kuaraina2"]
 
-  
-  if message == "oi":
-     texto_resposta = f"teste"
-  elif message == "1":
-     texto_resposta = f"teste 2"
-  else:
-     texto_resposta = f"teste 3"
+        # Loop para processar cada atualização recebida
+        for update in dados:
+            update_id = update["update_id"]
 
-  nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
-  resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data = nova_mensagem)
-  return "ok"
+            # Verifica se o usuário é permitido
+            if "username" in update["message"]["from"] and update["message"]["from"]["username"] in usuarios_permitidos:
+                # Extrai dados para mostrar mensagem recebida
+                first_name = update["message"]["from"]["first_name"]
+                sender_id = update["message"]["from"]["id"]
+                if "text" not in update["message"]:
+                    continue  # Essa mensagem não é um texto!
+                message = update["message"]["text"]
+                chat_id = update["message"]["chat"]["id"]
+                datahora = str(datetime.datetime.fromtimestamp(update["message"]["date"]))
+                username = update["message"]["from"]["username"] if "username" in update["message"]["from"] else "[não definido]"
+                print(f"[{datahora}] Nova mensagem de {first_name} @{username} ({chat_id}): {message}")
+                mensagens.append([datahora, "recebida", username, first_name, chat_id, message])
+
+                # Define qual será a resposta e envia
+                if message == "/start":
+                    texto_resposta = "Este é um robô privado para envio de conteúdo sensível."
+                else:
+                    texto_resposta = "texto resposta" #**************************** AQUI ENTRA A INTEGRAÇÃO COM GSHEETS E SENDGRID???????
+                nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
+                requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+                mensagens.append([datahora, "enviada", username, first_name, chat_id, texto_resposta])
+            else:
+                texto_resposta_negado = "Você não tem permissão para utilizar esse serviço."
+                # Se o usuário não for permitido, pula para a próxima atualização
+                continue
+
