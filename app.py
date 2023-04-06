8137880a3dde62b9419cfd4585d8eff172583c34
Karina Ferreira <108735210+karina-ferreira@users.noreply.github.com>
2023-04-06 14:53:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,3 +38,46 @@ def index():
   return "Esse é o site do Bot Assessor de Mailing. Para conferir a documentação acesse: https://github.com/karina-ferreira/bot_assessor_mailing"
 
 # ______________________________ bot ___________________________________________
+
+@app.route("/telegram-bot", methods=['POST'])
+def telegram_bot():
+  mensagens = []
+
+  update = request.json
+  
+  if request.method == 'POST':
+     update = request.get_json()
+    
+  update_id = update['update_id']
+  first_name = update['message']['from']['first_name']
+  last_name = update['message']['from']['last_name']
+  user_name = update['message']['from']['username']
+  sender_id = update['message']['from']['id']
+  date = datetime.fromtimestamp(update['message']['date']).date()
+  time = datetime.fromtimestamp(update['message']['date']).time()
+  chat_id = update['message']['chat']['id']
+  
+  try:
+      message = update['message']['text']
+  except KeyError:
+      print("erro")
+      message=''
+
+  #return chat_id, texto
+  
+  if "username" in update["message"]["from"]:
+    username = f' @{update["message"]["from"]["username"]}'
+  else:
+    username = ""
+
+  
+  if message == "oi":
+     texto_resposta = f"teste"
+  elif message == "1":
+     texto_resposta = f"teste 2"
+  else:
+     texto_resposta = f"teste 3"
+
+  nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
+  resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data = nova_mensagem)
+  return "ok"
