f33b9d6dcb4e730827b81d47977e1afb83cf8b7c
Karina Ferreira <108735210+karina-ferreira@users.noreply.github.com>
2023-04-07 10:51:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -37,49 +37,32 @@ app = Flask(__name__)
 def index():
   return "Esse é o site do Bot Assessor de Mailing. Para conferir a documentação acesse: https://github.com/karina-ferreira/bot_assessor_mailing"
 
-# ______________________________ bot (teste quase completo) ____________________
+# ______________________________ bot (teste 2 refazendo o código) ____________________
 
 @app.route("/telegram-bot", methods = ["POST"])
 def telegram_bot():
     mensagens = []
   
-    if request.method == 'POST':
-        update = request.json
+    update = request.json 
 
-        # Defina a lista de usuários permitidos
-        usuarios_permitidos = []
-        #usuarios_permitidos = ["kuaraina", "kuaraina2"]
+    # dados da mensagem
+    update_id = update['update_id']
+    first_name = update['message']['from']['first_name']
+    last_name = update['message']['from']['last_name']
+    user_name = update['message']['from']['username']
+    sender_id = update['message']['from']['id']
+    chat_id = update['message']['chat']['id']
+    date = datetime.fromtimestamp(update['message']['date']).date().strftime('%d/%m/%Y')
+    time = datetime.fromtimestamp(update['message']['date']).time()
 
-        # Loop para processar cada atualização recebida
-        for cada_update in update:
-            update_id = cada_update["update_id"]
+    # Define qual será a resposta e envia
+    if message == "/start":
+        texto_resposta = "Este é um robô privado para envio de conteúdo sensível."
+    else:
+        texto_resposta = "estou em fase de testes!" #**************************** AQUI ENTRA A INTEGRAÇÃO COM GSHEETS E SENDGRID / fazer funções
+        nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
+        requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+        mensagens.append([datahora, "enviada", username, first_name, chat_id, texto_resposta])
 
-            # Verifica se o usuário é permitido
-            if "username" in cada_update["message"]["from"] and cada_update["message"]["from"]["username"] in usuarios_permitidos:
-                # Extrai dados para mostrar mensagem recebida
-                first_name = cada_update["message"]["from"]["first_name"]
-                sender_id = cada_update["message"]["from"]["id"]
-                if "text" not in cada_update["message"]:
-                    continue  # Essa mensagem não é um texto!
-                message = cada_update["message"]["text"]
-                chat_id = cada_update["message"]["chat"]["id"]
-                datahora = str(datetime.datetime.fromtimestamp(cada_update["message"]["date"]))
-                username = cada_update["message"]["from"]["username"] if "username" in cada_update["message"]["from"] else "[não definido]"
-                print(f"[{datahora}] Nova mensagem de {first_name} @{username} ({chat_id}): {message}")
-                mensagens.append([datahora, "recebida", username, first_name, chat_id, message])
-
-                # Define qual será a resposta e envia
-                if message == "/start":
-                    texto_resposta = "Este é um robô privado para envio de conteúdo sensível."
-                else:
-                    texto_resposta = "texto resposta" #**************************** AQUI ENTRA A INTEGRAÇÃO COM GSHEETS E SENDGRID???????
-                nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
-                requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-                mensagens.append([datahora, "enviada", username, first_name, chat_id, texto_resposta])
-            else:
-                texto_resposta_negado = "Você não tem permissão para utilizar esse serviço."
-                # Se o usuário não for permitido, pula para a próxima atualização
-                
-                continue
-                
     print(resposta.text)
+    return "ok"
