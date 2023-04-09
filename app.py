24a6f832a540862d4104923fbdcc41b4fc4f918f
Karina Ferreira <108735210+karina-ferreira@users.noreply.github.com>
2023-04-09 20:13:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,16 @@ from flask import Flask, request
 from io import StringIO 
 from oauth2client.service_account import ServiceAccountCredentials
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import Mail, Email, To, Content
 
 import obter_info_msg
+import enviar_email
+import funcoes_separacao
 
 # ______________________________ variáveis de ambiente _________________________
 
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
-SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY"] 
+SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY_SITE"] 
 GOOGLE_SHEETS_KEY = os.environ["GOOGLE_SHEETS_KEY"]
 GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"] 
 with open("credenciais.json", mode="w") as arquivo:
@@ -37,14 +39,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "Esse é o site do Bot Assessor de Mailing. Para conferir a documentação acesse: https://github.com/karina-ferreira/bot_assessor_mailing"
+  return "Esse é o site de teste"
 
-# ______________________________ bot (teste usuários permitidos) _____________________________
+# ______________________________ bot (FUNCIONANDO) _____________________________
 
 @app.route("/telegram-bot", methods = ["POST"])
 def telegram_bot():
-
-    update = request.json
+  
+    update = request.json 
 
 # Dados da mensagem
     update_id = update['update_id']
@@ -56,33 +58,30 @@ def telegram_bot():
     message = update["message"]["text"]
     date = datetime.fromtimestamp(update['message']['date']).date()
     time = datetime.fromtimestamp(update['message']['date']).time()
+    
+# Funções
+    lista_msg = obter_info_msg.obter_info_msg(message)
+    emails = funcoes_separacao.obter_mailing(lista_msg)
+    titulo = funcoes_separacao.obter_titulo(lista_msg)
+    comentario = funcoes_separacao.obter_comentario(lista_msg)
+    resultado_envios = enviar_email.enviar_email(emails, titulo, comentario)
 
-# Só responde textos
-    try:
-      message = update['message']['text']
-    except KeyError:
-      texto_resposta = "Esse bot suporta apenas conteúdo em texto."
-      nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
-      requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-      return "ok"
-
+# Define qual será a resposta
+    texto_resposta = ""
+    if message == "/start":
+        texto_resposta = "Este é um robô privado para envio de conteúdo sensível."
+    else:
+        texto_resposta = resultado_envios
+        
 # Define a lista de usuários permitidos
-    usuarios_permitidos = ["kuaraina", "kuaraina2"]
+    usuarios_permitidos = ["kuaraina", "kuaraina2"]    
 
 # Verifica se o usuário está na lista de usuários permitidos e define resposta para não permitidos
     if not user_name or user_name not in usuarios_permitidos:
         texto_resposta = "Você não está autorizado a usar este bot."
     
-# Define qual será a resposta
-    if message == "/start":
-        texto_resposta = "Este é um robô privado para envio de conteúdo sensível."
-    else:
-        texto_resposta = "estou em fase de testes!" #obter_info_msg(update) #é aqui que deve entrar? como?
-        
 # Envia a resposta       
     nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
     requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
 
     return "ok"
-  
-# ______________________________ integração com SendGrid e GSheets _____________________________
