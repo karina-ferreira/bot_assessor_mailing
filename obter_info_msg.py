9469bfa9bc7eb17f7453f54121879eb44c29c8ec
Karina Ferreira <108735210+karina-ferreira@users.noreply.github.com>
2023-04-09 20:15:04 -0300

    Update obter_info_msg.py

--- obter_info_msg.py
+++ obter_info_msg.py
@@ -1,16 +1,43 @@
-categoria = 0
-titulo = 0
-comentario = 0
+import os
 
+import gspread
+import json
+import requests
+import sendgrid
+import time
+
+from datetime import date, time
+from datetime import datetime
+from flask import Flask, request
+from io import StringIO 
+from oauth2client.service_account import ServiceAccountCredentials
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, Email, To, Content
+
+# ______________________________ variáveis de ambiente _________________________
+
+TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
+SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY_SITE"] 
+GOOGLE_SHEETS_KEY = os.environ["GOOGLE_SHEETS_KEY"]
+GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"] 
+with open("credenciais.json", mode="w") as arquivo:
+  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+  
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+api = gspread.authorize(conta)
+planilha = api.open_by_key(f'{GOOGLE_SHEETS_KEY}')
+sheet_recebidas = planilha.worksheet("comentarios") 
+sheet_mailing = planilha.worksheet("mailing") 
+
+# ______________________________ função ____________________________
+
+# pega a mensagem recebida pelo bot, divide em 3 partes e retorna uma lista
 def obter_info_msg(message):
+    message = message.split("/")
 
-    global categoria
-    global titulo
-    global comentario
-    partes_mensagem = message.split("/")
-    if len(partes_mensagem) == 3:
-        categoria, titulo, comentario = partes_mensagem
-        return categoria, titulo, comentario, None
+    if len(message) == 3:
+        lista_msg = message
+        return lista_msg
     else:
         texto_resposta = "A mensagem deve ter o seguinte formato: 'número da categoria (1-6)/título do comentário/comentário em si', separado por barras."
         return None, None, None, texto_resposta
