59e36e272b124c9bf1279f4f4b372ac732050a12
Karina Ferreira <108735210+karina-ferreira@users.noreply.github.com>
2023-04-10 01:36:09 -0300

    Update obter_info_msg.py

--- obter_info_msg.py
+++ obter_info_msg.py
@@ -1,36 +1,3 @@
-import os
-
-import gspread
-import json
-import requests
-import sendgrid
-import time
-
-from datetime import date, time
-from datetime import datetime
-from flask import Flask, request
-from io import StringIO 
-from oauth2client.service_account import ServiceAccountCredentials
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail, Email, To, Content
-
-# ______________________________ variáveis de ambiente _________________________
-
-TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
-SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY_SITE"] 
-GOOGLE_SHEETS_KEY = os.environ["GOOGLE_SHEETS_KEY"]
-GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"] 
-with open("credenciais.json", mode="w") as arquivo:
-  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
-  
-conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
-api = gspread.authorize(conta)
-planilha = api.open_by_key(f'{GOOGLE_SHEETS_KEY}')
-sheet_recebidas = planilha.worksheet("comentarios") 
-sheet_mailing = planilha.worksheet("mailing") 
-
-# ______________________________ função ____________________________
-
 # pega a mensagem recebida pelo bot, divide em 3 partes e retorna uma lista
 def obter_info_msg(message):
     message = message.split("/")
