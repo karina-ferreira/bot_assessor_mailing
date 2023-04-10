06014c792596a93990c2bb2d1cbbe82295b81419
Karina Ferreira <108735210+karina-ferreira@users.noreply.github.com>
2023-04-10 18:57:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def telegram_bot():
     
 # Envia a resposta 
 
-      nova_mensagem = {"chat_id": chat_id, "text": resposta} 
-      requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+    nova_mensagem = {"chat_id": chat_id, "text": resposta} 
+    requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
 
     return "ok"
